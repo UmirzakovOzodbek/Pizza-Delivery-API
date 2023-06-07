595c6e9c48faf82a5597110f0b6e3541dd1b7ee8
user <ozodbekumurzakov2006@gmail.com>
2023-06-07 16:41:50 +0500

    .gitignore

--- database.py
+++ database.py
@@ -1,7 +1,15 @@
+from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('postgresql://postgres:123456ozod@localhost/pizza_db', echo=True)
+env = dotenv_values(".env")
+
+# engine = create_engine('postgresql://postgres:123456ozod@localhost/pizza_db', echo=True)
+DB_NAME = env.get("DB_NAME")
+DB_USER = env.get("DB_USER")
+DB_PASSWORD = env.get("DB_PASSWORD")
+SQL_ALCHEMY_DB_URL = f'sqlite:///./app.db'
+engine = create_engine(SQL_ALCHEMY_DB_URL, connect_args={"check_same_thread": False}, echo=True)
 
 Base = declarative_base()
 
